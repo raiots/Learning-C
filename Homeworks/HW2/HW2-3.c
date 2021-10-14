2914db101d33a4a656342bcb618089f0a3093388
raiots <raiot.lee@hotmail.com>
2021-10-14 18:59:17 +0800

    fix bug about identify isosceles right triangle & change int to float

--- Homeworks/HW2/HW2-3.c
+++ Homeworks/HW2/HW2-3.c
@@ -10,23 +10,24 @@ Date: 2021-10-13
 
 int main()
 {
-    int a, b, c;
-    printf("Please input three side length, like '3 4 5':\n");
-    scanf("%d %d %d", &a, &b, &c);
-    if(a==b || b==c || a==c){
-        if(a == b == c){
-            printf("This is an equilateral triangle.");
-        }
-        else{
-            printf("This is an isosceles.");
-        }
-    }
-    else if(a*a + b*b == c*c || b*b + c*c == a*a || a*a + c*c == b*b){
+    float a, b, c;
+    printf("Please input three side length, like '3.2 4.1 5':\n");
+    scanf("%f %f %f", &a, &b, &c);
+    // Must run thhis at first.
+    if(a*a + b*b == c*c || b*b + c*c == a*a || a*a + c*c == b*b){
         if(a==b || b==c || a==c){
             printf("This is an isosceles right triangle.");
         }
         else{
             printf("This is a right triangle.");
+        }
+    }
+    else if(a==b || b==c || a==c){
+        if(a == b == c){
+            printf("This is an equilateral triangle.");
+        }
+        else{
+            printf("This is an isosceles.");
         }
     }
     else{
