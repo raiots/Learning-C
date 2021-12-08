6746bab23f9b97cb11285cb3437a4ecfb64c2122
raiots <raiot.lee@hotmail.com>
2021-12-08 22:06:41 +0800

    add: HW10 alpha

---
+++ Homeworks/HW10/HW10-1.c
@@ -0,0 +1,56 @@
+/*****************************************************************************
+File name: HW10-1.c
+Description: 利用指针函数，对字符串数组排序
+Permalink: https: //github.com/raiots/Learning-C/tree/main/Homeworks/HW10
+Author: Raiot.Me
+Date: 2021-12-8
+*****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int isLower(char *str1, char *str2)
+{
+    return strcmp(str1, str2) < 0;
+}
+
+int isLarger(char *str1, char *str2)
+{
+    return strcmp(str1, str2) > 0;
+}
+
+void stringSort(char *str[], int n, int (*compare)(char*, char*))
+{
+    int i, j;
+    char *temp;
+    for (i=n-1; i>0; i--){
+        for (j=0; j<i; j++){
+            if (compare(str[j], str[j+1])){
+                temp = str[j];
+                str[j] = str[j+1];
+                str[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(char *str[], int n)
+{
+    int i;
+    for (i=0; i<n; i++)
+        printf("%s\n", str[i]);
+}
+
+int main()
+{
+    char *str[3];
+    str[0] = "cat";
+    str[1] = "cat";
+    str[2] = "mouse";
+
+    printArray(str, 3);
+    stringSort(str, 3, isLower);
+    printArray(str, 3);
+
+    return 0;
+}
---
+++ Homeworks/HW10/HW10-2.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+File name: HW10-1.c
+Description: 利用指针函数，对字符串数组排序
+Permalink: https: //github.com/raiots/Learning-C/tree/main/Homeworks/HW10
+Author: Raiot.Me
+Date: 2021-12-8
+*****************************************************************************/
+
+int main()
+{
+    printf("Hello");
+
+    return 0;
+}
